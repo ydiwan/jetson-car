766cf269b5d06fcbb8f0149d8764392f0dcd8e29
Youssef Diwan <diwany@vcu.edu>
2026-04-05 17:10:35 -0400

    changed vicon converter to best effort to match vicon bridge:

--- src/sensor_fusion/sensor_fusion/vicon_converter_node.py
+++ src/sensor_fusion/sensor_fusion/vicon_converter_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped, TransformStamped
 from tf2_ros import TransformBroadcaster
+from rclpy.qos import qos_profile_sensor_data
 
 class ViconTFBroadcaster(Node):
     def __init__(self):
@@ -10,32 +11,28 @@ class ViconTFBroadcaster(Node):
         # Initialize broadcaster
         self.tf_broadcaster = TransformBroadcaster(self)
         
-        # Subscription
+        # Subscriptions
         self.subscription = self.create_subscription(
             PoseStamped,
             'vicon_pose', 
             self.vicon_callback,
-            10)
+            qos_profile_sensor_data)
             
         self.get_logger().info("Vicon TF Bridge active: Translating vicon_pose to odom -> base_link")
 
     def vicon_callback(self, msg):
         t = TransformStamped()
 
-        # Read time and frame id
         t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = 'odom'
         t.child_frame_id = 'base_link'
 
-        # Copy translation (x, y, z)
         t.transform.translation.x = msg.pose.position.x
         t.transform.translation.y = msg.pose.position.y
         t.transform.translation.z = msg.pose.position.z
 
-        # Copy rotation
         t.transform.rotation = msg.pose.orientation
 
-        # Broadcast transform
         self.tf_broadcaster.sendTransform(t)
 
 def main(args=None):
