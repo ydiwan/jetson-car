5edd62b3ed28d029da4c248184de859448c8c1bb
Youssef Diwan <youssefdiwan08@gmail.com>
2026-04-14 02:50:24 -0400

    tf arg fix

--- src/vehicle_bringup/launch/hardware.launch.py
+++ src/vehicle_bringup/launch/hardware.launch.py
@@ -130,7 +130,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='static_map_to_odom',
-            arguments=['0', '0', '0', '1.5708', '0', '0', 'map', 'odom'],
+            arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom'],
             parameters=[use_sim_time_param]
     )
 
